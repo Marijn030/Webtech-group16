1b5e4c42d64e7305e72858eebfb14b0e2c8e5ace
Jonathan van Dijk <jonathane.vandijk@gmail.com>
2023-03-24 21:32:06 +0100

    Adding in morgan logger?

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+//declaring different middlewares to use
 var app = require('express')();
+var morgan = require('morgan');
+
+//adding lisening
 app.get("/", function(req, res){
+    res.writeHead(200);
     res.end("Test Test server");
 });
 app.listen(8016);
