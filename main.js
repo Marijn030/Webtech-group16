34dddeaa8e12838c7538087227b0b5863b900408
DESKTOP-GUV8SCB\tayla <t.batman@students.uu.nl>
2023-03-27 12:14:18 +0200

    added static file

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //declaring different middlewares to use
 var app = require('express')();
 var morgan = require('morgan');
-
+var path = require("path");
 //adding lisening
 app.get("/", function(req, res){
     res.writeHead(200);
