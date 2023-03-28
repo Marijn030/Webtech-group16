6fa19fb561bdb3ca7cc55910868ca082323663f5
tbatman <taylan.bat@icloud.com>
2023-03-28 22:05:23 +0200

    added static files in 2 ways

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 //declaring different middlewares to use
-var app = require('express')();
+var express = require('express');
+var app = express();
 var morgan = require('morgan');
 var path = require("path");
+var fs = require("fs");
+
 //adding lisening
 app.get("/", function(req, res){
-    res.writeHead(200);
-    res.end("Test Test server");
+    fs.readFile('static/web_pages/index.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
 });
+
+var staticPath = path.join(__dirname, "/static");
+app.use(express.static(staticPath));
+
 app.listen(8016);
