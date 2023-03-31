0f63e63b001242c7502de12d5e8ae00cb55fc47e
tbatman <taylan.bat@icloud.com>
2023-03-31 22:27:12 +0200

    added nav

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,14 +1,20 @@
-function createh2(link, name, optionalClass) {
-    let h2 = document.createElement('h2');
+function createli(link, name, optionalClass) {
+    let li = document.createElement('li');
     let a = document.createElement('a');
     a.setAttribute('href', link);
     if (optionalClass != null) { a.setAttribute('class', optionalClass); }
     let text = document.createTextNode(name);
     a.appendChild(text);
-    h2.appendChild(a);
-    return h2;
+    li.appendChild(a);
+    return li;
 }
 
 let nav = document.getElementsByTagName('nav')[0];
+let ul = document.createElement('ul');
+let login = (createli('login.html', 'Login', null));
+login.style.cssFloat = 'right';
+ul.append(createli('index.html', 'Home', null));
+ul.append(createli('index.html', 'Home', null));
+ul.append(login);
 
-nav.appendChild(createh2('index.html', 'Home', null));
+nav.appendChild(ul);
