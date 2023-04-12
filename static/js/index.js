1d1ec2e8de62a98a51cb967d6a04ec2226792670
MarijnLuime <m.d.luime@student.tudelft.nl>
2023-04-12 16:51:12 +0200

    fixed pagination

---
+++ static/js/index.js
@@ -0,0 +1,46 @@
+var next = document.getElementsByTagName('button')[10];
+next.addEventListener("click", nextMovie, false);
+
+function nextMovie() {
+    var h3 = document.getElementsByTagName('h3')[0];
+
+    var text = String(h3.innerText);
+    var strings = text.split(' ');
+
+    var currentSet = parseInt(strings[1])+1;
+    var totalSets = parseInt(strings[3]);
+
+    h3.innerText = "Set " + currentSet + " of " + totalSets;
+
+    reloadPage();
+}
+
+function reloadPage() {
+    fetch("/movies").then(c => c.json()).then(movies => {
+        console.log(movies);
+
+        var h3 = document.getElementsByTagName('h3')[0];
+        var text = String(h3.innerText);
+        var strings = text.split(' ');
+
+        var currentSet = parseInt(strings[1]);
+        var totalSets = parseInt(strings[3]);
+
+        if (currentSet > totalSets) {
+            currentSet = 1;
+        }
+
+        var h3 = document.getElementsByTagName('h3')[0];
+        h3.innerText = "Set " + currentSet + " of " + totalSets;
+
+        var increment = currentSet - 1;
+        var begin = 0 + 10 * increment;
+
+        var labels = document.getElementsByTagName('label');
+        var buttons = document.getElementsByTagName('button');
+        for (let i = begin; i < begin + 10; i++) {
+            labels[i % 10].innerText = "Movie " + movies[i].id;
+            buttons[i % 10].innerText = movies[i].title;
+        }
+    });
+}
