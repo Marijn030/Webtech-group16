c802c467e74b860fe52a167c251a0ca33f9200d8
MarijnLuime <m.d.luime@student.tudelft.nl>
2023-04-12 16:26:46 +0200

    movies display fix

--- static/js/movies.js
+++ static/js/movies.js
@@ -1,10 +1,27 @@
 fetch("/movies").then(c => c.json()).then(movies => {
     console.log(movies);
 
+    var h3 = document.getElementsByTagName('h3')[0];
+    var text = String(h3.innerText);
+    var strings = text.split(' ');
+
+    var currentSet = parseInt(strings[1]);
+    var totalSets = parseInt(strings[3]);
+
+    if (currentSet > totalSets) {
+        currentSet = 1;
+    }
+
+    var h3 = document.getElementsByTagName('h3')[0];
+    h3.innerText = "Set " + currentSet + " of " + totalSets;
+
+    var increment = currentSet - 1;
+    var begin = 0 + 10 * increment;
+
     var labels = document.getElementsByTagName('label');
     var buttons = document.getElementsByTagName('button');
-    for (let i = 0; i < 10; i++) {
-        labels[i].innerText = "Movie " + movies[i].id;
-        buttons[i].innerText = movies[i].title;
+    for (let i = begin; i < begin+10; i++) {
+        labels[i%10].innerText = "Movie " + movies[i].id;
+        buttons[i%10].innerText = movies[i].title;
     }
 });
