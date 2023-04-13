bfae3977cf6e7ddd0ac2f7cacce4b934ba4dfba3
tbatman <taylan.bat@icloud.com>
2023-04-13 21:12:57 +0200

    added new file + deleted useless code

--- main.js
+++ main.js
@@ -44,14 +44,7 @@ app.get("/static/web_pages/userprofile.html", function (req, res) {
         //can be visited at http://localhost:8016
     });
 });
-app.get("/static/web_pages/moviefastandfurious1.html", function (req, res) {
-    fs.readFile('static/web_pages/moviefastandfurious1.html', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-        //can be visited at http://localhost:8016
-    });
-});
+
 
 var staticPath = path.join(__dirname, "/static");
 app.use(express.static(staticPath)); //makes you able to acces static files such as the css/img/js. NOTE: HTML/PUG isn't static because we pull data from DB. 
