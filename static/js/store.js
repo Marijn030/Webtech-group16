e2d1b775a3406b7ef8245e33353531d8ba416187
MarijnLuime <m.d.luime@student.tudelft.nl>
2023-04-14 17:55:49 +0200

    added nav in store

--- static/js/store.js
+++ static/js/store.js
@@ -3,5 +3,6 @@ var select = document.getElementsByTagName('select')[0];
 select.addEventListener('change', movieSelected, false);
 
 function movieSelected() {
-
+    //let label = document.getElementsByTagName('label')[1];
+    //label.innerText = "You selected the moviescreening: " + this.childNodes[0].innerText;
 }
