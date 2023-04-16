3baf6564674a57e025661e2a1714573a4e76dce5
Jonathan van Dijk <jonathane.vandijk@gmail.com>
2023-04-16 17:46:29 +0200

    added more comments to the files included. removed a variable declaration in movies.js, because variable already existed movies.js still not entirely finished. I do not understand all decisions made there

--- static/js/movies.js
+++ static/js/movies.js
@@ -3,22 +3,21 @@ it also renders the different parts about the button that loads the next 10 movi
 */
 //get all the movies to display on the home page index.html
 fetch("/movies").then(c => c.json()).then(movies => {
-    var h3 = document.getElementsByTagName('h3')[0];//
-    var text = String(h3.innerText);
-    var strings = text.split(' '); //array of the strings that contains
+    var h3 = document.getElementsByTagName('h3')[0];//the header that tells the user what set they are on (which 10 movies)
+    var text = String(h3.innerText);//the text that is contained in the defined header h3 element
+    var strings = text.split(' '); //array where the text is split up so the current set can be retrieved off the html page
 
-    var currentSet = parseInt(strings[1]);
-    var totalSets = Math.ceil(movies.length / 10);
+    var currentSet = parseInt(strings[1]);//gives the number associated with the current set
+    var totalSets = Math.ceil(movies.length / 10);//number of total sets of movies in the database
 
     if (currentSet > totalSets) {
         currentSet = 1;
     }
 
-    var h3 = document.getElementsByTagName('h3')[0];
     h3.innerText = "Set " + currentSet + " of " + totalSets;
 
-    var increment = currentSet - 1;
-    var begin = 0 + 10 * increment;
+    var increment = currentSet - 1;//
+    var begin = 0 + 10 * increment;//
 
     var labels = document.getElementsByTagName('label');
     var buttons = document.getElementsByTagName('button');
